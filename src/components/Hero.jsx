6e8c8c1d021f03fd272e2c4def19ca1177f58768
CulturalProfessor <svinayak994@gmail.com>
2023-10-11 02:45:04 +0530

    fix : responsive layout

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,22 +4,19 @@ import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
-    // <div className=" p-6 md:columns-2 ">
-    <div className="flex mt-10">
-      <img src={food} alt="food" className="w-1/2" />
-      <div className="flex flex-col gap-5 justify-evenly">
-        <h1 className="text-5xl text-left text-left font-bold pr-3">
+    <div className="flex flex-col md:flex-row mt-10">
+      <img src={food} alt="food" className="w-full md:w-1/2" />
+      <div className="flex flex-col justify-center md:pl-8">
+        <h1 className="text-3xl md:text-5xl font-bold">
           Experience the taste from around the world
         </h1>
-        <p className="text-lg leading-9 text-justify pr-8">
+        <p className="text-base md:text-lg leading-6 text-justify mt-2">
           The world is full of tasty dishes in different types of cuisine to
           capture the different cooking styles, traditions, and cultures.
-          Explore and try out recipes from from all around the world.
+          Explore and try out recipes from all around the world.
         </p>
-        {/* make a sign up button at the  */}
-        {/* mt-10 ml-40  px-6 py-2 */}
         <Link to="/about">
-          <button className="bg-[#507b14] text-white  text-center rounded-xl w-36 px-6 py-2 ">
+          <button className="bg-[#507b14] text-white text-center rounded-xl mt-4 md:mt-6 w-36 px-4 py-2">
             About Us
           </button>
         </Link>
