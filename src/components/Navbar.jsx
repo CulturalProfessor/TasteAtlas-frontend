9db716cbcd3ef30095ef85c33e4c9ee1e3ddc38a
CulturalProfessor <svinayak994@gmail.com>
2023-10-11 00:35:28 +0530

    fix : auth (5)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -79,7 +79,7 @@ const Navbar = (props) => {
               <button
                 onClick={() =>
                   loginWithRedirect({
-                    redirect_uri: "http://127.0.0.1:5173/dashboard",
+                    redirect_uri: "https://tasteatlas-frontend.onrender.com/dashboard",
                   })
                 }
               >
